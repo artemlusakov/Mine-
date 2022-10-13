7a160d0e28e232b95f82aa19a1b09f7591ad5029
artemlusakov <artemlysakov793@gmail.com>
2022-10-13 22:19:02 +0300

    Роли

--- Start.bat
+++ Start.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SET BINDIR=%~dp0
 CD /D "%BINDIR%"
-java -Xmx10240M -Xms10240M -Dfile.encoding=UTF-8 -jar paper-1.19.2-201.jar
+java -Xmx10240M -Xms1024M -Dfile.encoding=UTF-8 -jar paper-1.19.2-201.jar
 PAUSE
